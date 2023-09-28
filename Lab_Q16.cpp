53951ca2e55e378e0dc092afca8fd409c2c4b23c
chanu4321 <chanugupta2012@gmail.com>
2023-09-28 23:32:30 +0530

    pending

--- Lab_Q16.cpp
+++ Lab_Q16.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 class Person{
@@ -80,4 +80,4 @@ int main(){
     Person1.CreateObject(1,PersonArray);
     Person1.DisplayObjects(PersonArray,1);
     return 0;
-}
+} */
