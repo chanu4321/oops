52a104b30d00c983252cbf24fb6135db8d42ba2e
chanu4321 <chanugupta2012@gmail.com>
2023-10-08 00:00:08 +0530

    Q23 done

---
+++ Lab_Q23.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class A {
+    public:
+    A () {
+        cout << "base class contructor called" << endl;
+    }
+};
+
+// class B  : public A  {
+//     public:
+//     B () {
+//         cout << "single inheritance class contructor called" << endl;
+//     }
+// };
+// //  class C : public A, public B {
+// //     public:
+// //     C () {
+// //         cout << "multiple inheritance class contructor called" << endl;
+// //     }
+// // };
+// // class D : public B {
+// //     public:
+// //     D () {
+// //         cout << "multilevel inheritance class contructor called" << endl;
+// //     }
+// // };
+// class E : public A {
+//     public:
+//     E () {
+//         cout << "hierarchical class contructor called" << endl;
+//     }
+// };
+              
+int main() {
+    cout << "Chaitanya Gupta" << endl;
+    cout << "A2305222276" << endl;
+
+    //B::A a1;
+    //C::A a1;
+    //D::A a1;
+    //E::A a1;
+    return 0;
+}
